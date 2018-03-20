00b2a928d46ffcbf3d8023d18049ef50a2483cc5
Paulo Vargatt <pvargatt@gmail.com>
2018-03-20 00:13:21 -0300

    observables

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {OfertasService} from '../ofertas.service';
-
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx'
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -27,7 +28,30 @@ export class OfertaComponent implements OnInit {
         this.oferta = oferta
       });
 
+/*
+    this.route.params.subscribe((param) => {
+      console.log(param)
+    },(error) => {
+      console.log(error)
+    },() => console.log('completeedd'))
+  }
+*/
+/*
+    let tempo = Observable.interval(5000)
+    tempo.subscribe((interval) => {
+      console.log(interval)
+    })
+    */
+
+
+    let meuObservableTeste = Observable.create((observer) => {
+      observer.next('primeiro evento')
+    });
+
+    meuObservableTeste.subscribe(
+      (resul) => console.log(resul)
+    )
+
 
   }
-
 }
